7bd6e78e7d4f07af6f541491d435e9450df00976
miyazaki761 <miyazaki.a0806@gmail.com>
2019-11-25 06:32:26 +0000

    [Start]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,12 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
+  end
+
+  def create
   end
 
   def edit
