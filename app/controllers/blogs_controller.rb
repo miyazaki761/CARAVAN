52e48327d31b0d8e5377a8893472059e78de3e2c
a <miyazaki.a0806@gmail.com>
2019-11-25 17:10:55 +0900

    [Finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,19 +6,22 @@ class BlogsController < ApplicationController
   end
 
   def new
-  	@blog = Blog.new(blog_params)
-  	blog.save
-  	redirect_to blogs_path
-  end
-
-  private
-  def blog_params
-    params.require(:blog).permit(:title, :category, :body)
+  	@blog = Blog.new
   end
 
   def create
+  	@blog = Blog.new(blog_params)
+  	@blog.save
+  	redirect_to blog_path
   end
 
   def edit
   end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
